ae7391e24176b6ce3c67297ca4aa101387f8c60d
Abdul salam <115148201+abdulsalam2540@users.noreply.github.com>
2024-05-30 11:37:37 +0400

    Add files via upload

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,10 @@
+# Define three separate strings
+str1 = "Hello"
+str2 = " Abdul Salam "
+str3 = "is here !"
+
+# Concatenate the strings
+output = str1 + str2 + str3
+
+# Print the concatenated result
+print(output)
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter the radius of the circle
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area of the circle
+area = 3.141592653589793 * radius * radius  # πr²
+
+# Print the area
+print("Here is the area of circle:", area)
